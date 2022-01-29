ad3f13030b67a80eb72cfb9ff6cd3415769ae9b9
Mari <sherst.neva.maria@ya.ru>
2022-01-29 18:43:39 +0300

    header done

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,51 @@
 <template>
-  <div id="nav">
-    <div>
-      <router-link :class="{active: $route.name === 'Main'}" to="/">Главная</router-link> |
-      <router-link :class="{active: $route.name === 'Services'}" to="/services">Услуги</router-link> |
-      <router-link :class="{active: $route.name === 'About'}" to="/about">О нас</router-link> |
-      <router-link :class="{active: $route.name === 'Contacts'}" to="/contacts">Контакты</router-link>
+  <div class="header">
+    <div class="header__wrapper">
+      <div class="logo">
+        <img src="@/assets/logo.png" alt="logo">
+      </div>
+      <div class="nav">
+        <div>
+          <router-link class="nav__link" :class="{active: $route.name === 'Main'}" to="/">Главная</router-link>
+          <router-link class="nav__link" :class="{active: $route.name === 'Services'}" to="/services">Услуги</router-link>
+          <router-link class="nav__link" :class="{active: $route.name === 'About'}" to="/about">О нас</router-link>
+          <router-link class="nav__link" :class="{active: $route.name === 'Contacts'}" to="/contacts">Контакты</router-link>
+        </div>
+      </div>
+      <div class="phone">
+        <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 401.998 401.998" xml:space="preserve">
+          <g>
+            <path d="M401.129,311.475c-1.137-3.426-8.371-8.473-21.697-15.129c-3.61-2.098-8.754-4.949-15.41-8.566
+              c-6.662-3.617-12.709-6.95-18.13-9.996c-5.432-3.045-10.521-5.995-15.276-8.846c-0.76-0.571-3.139-2.234-7.136-5
+              c-4.001-2.758-7.375-4.805-10.14-6.14c-2.759-1.327-5.473-1.995-8.138-1.995c-3.806,0-8.56,2.714-14.268,8.135
+              c-5.708,5.428-10.944,11.324-15.7,17.706c-4.757,6.379-9.802,12.275-15.126,17.7c-5.332,5.427-9.713,8.138-13.135,8.138
+              c-1.718,0-3.86-0.479-6.427-1.424c-2.566-0.951-4.518-1.766-5.858-2.423c-1.328-0.671-3.607-1.999-6.845-4.004
+              c-3.244-1.999-5.048-3.094-5.428-3.285c-26.075-14.469-48.438-31.029-67.093-49.676c-18.649-18.658-35.211-41.019-49.676-67.097
+              c-0.19-0.381-1.287-2.19-3.284-5.424c-2-3.237-3.333-5.518-3.999-6.854c-0.666-1.331-1.475-3.283-2.425-5.852
+              s-1.427-4.709-1.427-6.424c0-3.424,2.713-7.804,8.138-13.134c5.424-5.327,11.326-10.373,17.7-15.128
+              c6.379-4.755,12.275-9.991,17.701-15.699c5.424-5.711,8.136-10.467,8.136-14.273c0-2.663-0.666-5.378-1.997-8.137
+              c-1.332-2.765-3.378-6.139-6.139-10.138c-2.762-3.997-4.427-6.374-4.999-7.139c-2.852-4.755-5.799-9.846-8.848-15.271
+              c-3.049-5.424-6.377-11.47-9.995-18.131c-3.615-6.658-6.468-11.799-8.564-15.415C98.986,9.233,93.943,1.997,90.516,0.859
+              C89.183,0.288,87.183,0,84.521,0c-5.142,0-11.85,0.95-20.129,2.856c-8.282,1.903-14.799,3.899-19.558,5.996
+              c-9.517,3.995-19.604,15.605-30.264,34.826C4.863,61.566,0.01,79.271,0.01,96.78c0,5.135,0.333,10.131,0.999,14.989
+              c0.666,4.853,1.856,10.326,3.571,16.418c1.712,6.09,3.093,10.614,4.137,13.56c1.045,2.948,2.996,8.229,5.852,15.845
+              c2.852,7.614,4.567,12.275,5.138,13.988c6.661,18.654,14.56,35.307,23.695,49.964c15.03,24.362,35.541,49.539,61.521,75.521
+              c25.981,25.98,51.153,46.49,75.517,61.526c14.655,9.134,31.314,17.032,49.965,23.698c1.714,0.568,6.375,2.279,13.986,5.141
+              c7.614,2.854,12.897,4.805,15.845,5.852c2.949,1.048,7.474,2.43,13.559,4.145c6.098,1.715,11.566,2.905,16.419,3.576
+              c4.856,0.657,9.853,0.996,14.989,0.996c17.508,0,35.214-4.856,53.105-14.562c19.219-10.656,30.826-20.745,34.823-30.269
+              c2.102-4.754,4.093-11.273,5.996-19.555c1.909-8.278,2.857-14.985,2.857-20.126C401.99,314.814,401.703,312.819,401.129,311.475z"
+              />
+          </g>
+        </svg>
+        <a href="#">+7(700)001-18-88</a>
+      </div>
+      <div class="cart">
+        <svg width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M9 24C7.35 24 6.015 25.35 6.015 27C6.015 28.65 7.35 30 9 30C10.65 30 12 28.65 12 27C12 25.35 10.65 24 9 24ZM0 0V3H3L8.4 14.385L6.375 18.06C6.135 18.48 6 18.975 6 19.5C6 21.15 7.35 22.5 9 22.5H27V19.5H9.63C9.42 19.5 9.255 19.335 9.255 19.125L9.3 18.945L10.65 16.5H21.825C22.95 16.5 23.94 15.885 24.45 14.955L29.82 5.22C29.94 5.01 30 4.755 30 4.5C30 3.675 29.325 3 28.5 3H6.315L4.905 0H0ZM24 24C22.35 24 21.015 25.35 21.015 27C21.015 28.65 22.35 30 24 30C25.65 30 27 28.65 27 27C27 25.35 25.65 24 24 24Z" fill="#283837"/>
+        </svg>
+        <router-link :class="{active: $route.name === 'Cart'}" to="/cart">Корзина</router-link>
+      </div>
     </div>
-    <router-link :class="{active: $route.name === 'Cart'}" to="/cart">Корзина</router-link>
   </div>
   <router-view/>
 </template>
@@ -27,13 +66,6 @@
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
 html, body {
   padding: 0;
@@ -41,31 +73,47 @@ html, body {
   overflow-x: hidden;
 }
 
-body {
-  background-color: rgb(245, 245, 245);
-}
+.header {
+  background-color: #A6C6C0;
+  font-family: "Fira Sans", Arial, Helvetica, sans-serif;
+  font-size: 18px;
 
-#nav {
-  padding: 10px;
-  background-color: white;
-  line-height: 30px;
-  display: flex;
-  justify-content: space-between;
+  &__wrapper {
+    max-width: 1250px;
+    margin: 0 auto;
+
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
 
   a {
-    font-weight: bold;
-    color: darkgray;
+    color: #344948;
     text-decoration: none;
-    margin: 0 5px;
-    font-size: 1.25rem;
-
-    &.active {
-      color: #2c3e50;
-    }
   }
 }
 
-.text-center {
-  text-align: center;
+.logo {
+  width: 240px;
 }
+
+.nav {
+  &__link {
+    margin-right: 40px;
+  }
+}
+
+.phone,
+.cart {
+  display: flex;
+  align-items: center;
+
+  svg {
+    margin-right: 7px;
+    width: 30px;
+    height: 30px;
+    fill: #283837;
+  }
+}
+
 </style>
